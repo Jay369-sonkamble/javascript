ff636149a25fd8785e15b0782a35ced80e2d41a2
Jayshree123!@# <Jayshreesonkamble123!@#>
2021-02-10 22:32:22 +0530

    array commit

---
+++ arrays.js
@@ -0,0 +1,90 @@
+//let arr=[10,20,30,40,50,60,70,80,90,100];
+// console.log(arr);
+// console.log(arr.length);
+// arr.push(50);
+// console.log(arr);
+// arr.unshift(5);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.splice(3,2);
+// console.log(arr);
+// arr.splice(7,1);
+// console.log(arr);
+// arr.splice(0,2);
+// console.log(arr);
+// arr.splice(0,0,7,6,8,9);
+// console.log(arr);
+// arr.slice(3,4);
+// console.log(arr);
+// [arr.slice(4,8),arr.slice(0,4)];
+// console.log(arr);
+// //map function
+// console.log([10,20,30,40,50,60].map((Element,index)=>{
+//     return Element*10;
+// })
+// );
+
+// 
+
+//filter function
+// console.log(
+//     [10,20,30,40,50].filter((element,index)=>{
+//         return element<=30;
+//     })
+// );
+// console.log([10,20,30,40,50].map((element,index)=>{
+//     return element*20;
+// })
+// );
+
+// console.log([10,20,30,40,50].map((element,index)=>{
+//     return element*20;
+// }).filter((element,index)=>{
+//     return element<=700;
+
+// })
+// );
+//*Reduce
+// console.log([1,2,3,4,5].reduce((firstvalue,nextvalue)=>{
+//     return firstvalue+nextvalue;
+// })
+
+// );
+
+// console.log([1,2,3,4,5].map((element,index)=>{
+//     return element-1;
+// }).filter((element,index)=>{
+//     return element<=3;
+// })
+// );
+// //* reduceright
+// console.log(["angular11","to","welcome"].reduceRight((firstvalue,nextvalue)=>{
+//     return firstvalue+nextvalue;
+// })
+// );
+
+//some 
+// console.log(
+//     [10,20,30,40,50].some((Element,index)=>{
+//         return Element<=10;
+//     })
+// );
+//every
+// console.log(
+//     [10,20,30,40,50].every((Element,index)=>{
+//         return Element<=10;
+//     })
+// );
+
+//findindex
+// console.log(
+//     [10,20,30,40,50].findIndex((Element,index)=>{
+//         return Element===10;
+//     })
+// );
+
+
+
