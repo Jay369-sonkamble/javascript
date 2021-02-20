dbb86c0a6aff412c2a8bbba3298fa93e7a17ee76
Jayshree123!@# <Jayshreesonkamble123!@#>
2021-02-20 23:58:59 +0530

    basic javascript

---
+++ demo.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+console.log(10+"10");
+console.log(10-"10");
+console.log(10*"10");
+console.log(10/"10");
+console.log(100+ +"100")//for addition we can use the double + operator
